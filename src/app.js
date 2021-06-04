a8e811cc34f9b3bddc478960ec530cb32d3665f2
ALostCAWs <keighly@keighly.ca>
2021-06-04 12:25:57 -0400

    Improves display of the Chart of the Moment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 
 /* <-- Imports --> */
 import React, { useState, useEffect } from 'react';
-import { object, array, bool } from 'prop-types';
+import { object, array, bool, string, number } from 'prop-types';
 import {
   createInstances,
   // findStellia,
@@ -12,7 +12,7 @@ import {
 import './main.css';
 
 /* <-- Navigation UI --> */
-function Nav() {
+const Nav = () => {
   // Return UI
   return (
     <nav>
@@ -29,51 +29,36 @@ function Nav() {
   );
 }
 
-// const AstroDisplay = ({ year }) => {
-//   console.log(year);
+const GetMethods = ({ zodiac, houseSystem }) => {
+  const method = zodiac.charAt(0).toUpperCase() + zodiac.slice(1);
+  const system = houseSystem.charAt(0).toUpperCase() + houseSystem.slice(1);
+  return (
+    <p>Methods: {method} & {system}</p>
+  );
+}
 
-// const planets = horoscope._celestialBodies.all;
+const GetDegreesMinutes = ({ degreesFormatted }) => {
+  const deg = degreesFormatted.slice(0, 4).trim();
+  const minSec = degreesFormatted.slice(deg.length + 1, degreesFormatted.length).trim();
+  console.log(deg);
+  console.log(minSec);
+  return (
+    <p className='degrees'>{deg} {minSec}</p>
+  );
+}
 
-// console.log('forEach testing . . .');
-
-// const signArr = [];
-// const houseArr = [];
-// const signSet = new Set();
-// const houseSet = new Set();
-// planets.forEach(planet => {
-//   const name = planet.key;
-//   const sign = planet.Sign.key;
-//   const house = planet.House.id;
-
-//   console.log(`${name}      |   ${sign}   |   ${house}`);
-//   if (name !== 'sirius' && name !== 'chiron') {
-//     signArr.push(sign);
-//     houseArr.push(house);
-//     signSet.add(sign);
-//     houseSet.add(house);
-//   }
-// });
-
-// signSet.forEach(sign => {
-//   console.log(sign);
-//   const numOfSign = findStellia(signArr, sign);
-//   console.log(`${numOfSign} placements in ${sign}`);
-// });
-
-// houseSet.forEach(house => {
-//   console.log(house);
-//   const numOfHouse = findStellia(houseArr, house);
-//   console.log(`${numOfHouse} placements in house # ${house}`);
-// });
-
-//   // render() {
-//   return (
-//     <>
-//       <p>`Horoscope`</p>
-//     </>
-//   );
-//   // }
-// }
+const GetDateTimeLocation = ({ year, month, day, hour, minute, latitude, longitude }) => {
+  const dd = (day < 10) ? `0${day}` : `${day}`;
+  const mm = (month < 10) ? `0${month}` : `${month}`;
+  const date = `${dd}-${mm}-${year}`;
+  let time = (hour < 10) ? `0${hour}:${minute}` : `${hour}:${minute}`;
+  time = (hour < 12) ? time += `AM` : time += `PM`;
+  const EW = (longitude > 0) ? `E` : `W`;
+  const NS = (latitude > 0) ? `N` : `S`;
+  return (
+    <p>{date}, {time} at {Math.abs(latitude)}&deg; {NS}, {Math.abs(longitude)}&deg; {EW}</p>
+  );
+}
 
 const AstroDisplay = ({ horoscope, unknown }) => {
   /* <-- Initial Logs --> */
@@ -113,8 +98,8 @@ const AstroDisplay = ({ horoscope, unknown }) => {
 
   return (
     <>
-      <article>
-        <h2>Chart of The Moment</h2>
+      <article className='birth'>
+        <h2>Birth Chart</h2>
         <p>Zodiac: {horoscope._zodiac}</p>
         <p>House System: {horoscope._houseSystem}</p>
         {!unknown ? (
@@ -122,6 +107,83 @@ const AstroDisplay = ({ horoscope, unknown }) => {
         ) : (
           <>{planets.map((p) => (<p key={p.key}>{p.key} is in {p.Sign.key}</p>))}</>
         )}
+      </article>
+    </>
+  );
+}
+
+/* ---- Moment Display ---- */
+const ChartOfTheMoment = ({ horoscope }) => {
+  /* <-- Initial Logs --> */
+  console.log(`Chart of the Moment:`);
+  console.log(horoscope);
+  console.log(horoscope.origin);
+  console.log(horoscope._houseSystem);
+  console.log(`. . .`);
+
+  // /* <-- Variable Declarations --> */
+  const origin = horoscope.origin;
+  const planets = horoscope._celestialBodies.all;
+  // console.log(`${origin.year}-${origin.month}-${origin.date} ${origin.hour}:${origin.minute}`);
+
+  // /* <-- Planet, Sign, House Logs --> */
+
+  // const signArr = [];
+  // const houseArr = [];
+  // const signSet = new Set();
+  // const houseSet = new Set();
+  // planets.forEach(planet => {
+  //   const name = planet.key;
+  //   const sign = planet.Sign.key;
+  //   const house = planet.House.id;
+
+  //   console.log(`${name}      |   ${sign}   |   ${house}`);
+  //   if (name !== 'sirius' && name !== 'chiron') {
+  //     signArr.push(sign);
+  //     houseArr.push(house);
+  //     signSet.add(sign);
+  //     houseSet.add(house);
+  //   }
+  // });
+
+  // signSet.forEach(sign => {
+  //   console.log(sign);
+  //   const numOfSign = findStellia(signArr, sign);
+  //   console.log(`${numOfSign} placements in ${sign}`);
+  // });
+
+  // houseSet.forEach(house => {
+  //   console.log(house);
+  //   const numOfHouse = findStellia(houseArr, house);
+  //   console.log(`${numOfHouse} placements in house # ${house}`);
+  // });
+  /* <-- Return UI --> */
+  return (
+    <>
+      <article className='moment'>
+        <h2>Chart of The Moment</h2>
+        <div>
+          <GetDateTimeLocation
+            year={origin.year} month={origin.month} day={origin.date}
+            hour={origin.hour} minute={origin.minute}
+            latitude={origin.latitude}
+            longitude={origin.longitude}
+          />
+          <GetMethods
+            zodiac={horoscope._zodiac}
+            houseSystem={horoscope._houseSystem}
+          />
+        </div>
+        <div className='chart'>
+          <>{planets.map((p) => (
+            <>
+              <p className='planet' key={p.label}>{p.label}</p>
+              <p className='sign' key={p.label + p.Sign.label}>{p.Sign.label}</p>
+              <GetDegreesMinutes degreesFormatted={p.ChartPosition.Ecliptic.ArcDegreesFormatted30} />
+              <p className='house' key={p.label + p.House.id}>{p.House.id}</p>
+            </>
+          ))}</>
+        </div>
       </article>
     </>
   );
@@ -253,131 +315,65 @@ const AstroForm = ({ birthData, months, days, hours, minutes, zodiacs, houseSyst
 
   /* <-- Return UI --> */
   return (
-    <article>
-
-      {!submit ? (
-        <>
-          <h2>Enter Birth Info</h2>
-          <form action='' id='birth-data'>
-            <label htmlFor='year' className='date'>Year:</label>
-            <input type='number' id='year' className='date' name='year' value={formValues.year} onChange={handleChange} />
-
-            <label htmlFor='month' className='date'>Month:</label>
-            <select id='month' className='date' name='month' value={formValues.month} onChange={handleChange}>
-              {months.map((month) => <option key={month.value} value={month.value}>{month.label}</option>)}
-            </select>
-
-            <label htmlFor='day' className='date'>Day:</label>
-            <select id='day' className='date' name='day' value={formValues.day} onChange={handleChange}>
-              {days.map((day) => <option key={day} value={day}>{day}</option>)}
-            </select>
-
-            <label htmlFor='hour' className='time'>Hour:</label>
-            <select id='hour' className='time' name='hour' value={formValues.hour} onChange={handleChange} disabled={unknown}>
-              {hours.map((hour) => <option key={hour.value} value={hour.value}>{hour.label}</option>)}
-            </select>
-
-            <label htmlFor='minute' className='time'>Minute:</label>
-            <select id='minute' className='time' name='minute' value={formValues.minute} onChange={handleChange} disabled={unknown}>
-              {minutes.map((min) => <option key={min} value={min}>{min}</option>)}
-            </select>
-
-            <label htmlFor='unknown' className='time'>Unknown:</label>
-            <input type='checkbox' id='unknown' className='time' name='unknown' value={unknown} onChange={() => setUnknown(unknown => !unknown)}></input>
-
-            <label htmlFor='latitude' className='location'>Latitude:</label>
-            <input type='number' id='latitude' className='location' name='latitude' min='-90.00' max='90.00' value={formValues.latitude} onChange={handleChange} />
-            <label htmlFor='longitude' className='location'>Longitude:</label>
-            <input type='number' id='longitude' className='location' name='longitude' min='-90.00' max='90.00' value={formValues.longitude} onChange={handleChange} />
-            <button type='button' id='useLocation' className='location' name='useLocation' onClick={() => setLocation(useLocation => !useLocation)}>Use my Location</button>
-
-            <label htmlFor='zodiac' className='system'>Zodiac:</label>
-            <select id='zodiac' className='system' name='zodiac' value={formValues.zodiacType} onChange={handleChange}>
-              {zodiacs.map((zodiac) => <option key={zodiac.value} value={zodiac.value}>{zodiac.label}</option>)}
-            </select>
-
-            <label htmlFor='houseSystem' className='system'>House System:</label>
-            <select id='houseSystem' className='system' name='houseSystem' value={formValues.zodiacType} onChange={handleChange}>
-              {houseSystems.map((system) => <option key={system.value} value={system.value}>{system.label}</option>)}
-            </select>
-            <button type='button' id='submit' name='submit' onClick={() => setSubmit(true)}>Calculate Chart</button>
-          </form>
-        </>
-      ) : (
-        <CreateHoroscope chartData={birthData} />
-      )}
-      <CreateHoroscope chartData={MOMENTDATA} />
-    </article>
-  );
-}
-
-const ChartOfTheMoment = ({ horoscope }) => {
-  /* <-- Initial Logs --> */
-  console.log(`Chart of the Moment:`);
-  console.log(horoscope);
-  console.log(horoscope.origin);
-  console.log(horoscope._houseSystem);
-  console.log(`. . .`);
-
-  // /* <-- Date Logs --> */
-  // const origin = horoscope.origin;
-  // console.log(`${origin.year}-${origin.month}-${origin.date} ${origin.hour}:${origin.minute}`);
-
-  // /* <-- Planet, Sign, House Logs --> */
-  const planets = horoscope._celestialBodies.all;
-  // planets.forEach(planet => {
-  //   const name = planet.key;
-  //   const sign = planet.Sign.key;
-  //   const house = planet.House.id;
-  //   const degrees = planet.ChartPosition.Ecliptic.ArcDegreesFormatted30;
-  //   const output = planet.isRetrograde ? `${degrees}r` : `${degrees}`;
-
-  //   console.log(`${name}  |  ${sign}  | ${house}`);
-  //   console.log(output);
-  //   console.log(planet.ChartPosition);
-  // });
-
-  // console.log('forEach testing . . .');
-
-  // const signArr = [];
-  // const houseArr = [];
-  // const signSet = new Set();
-  // const houseSet = new Set();
-  // planets.forEach(planet => {
-  //   const name = planet.key;
-  //   const sign = planet.Sign.key;
-  //   const house = planet.House.id;
-
-  //   console.log(`${name}      |   ${sign}   |   ${house}`);
-  //   if (name !== 'sirius' && name !== 'chiron') {
-  //     signArr.push(sign);
-  //     houseArr.push(house);
-  //     signSet.add(sign);
-  //     houseSet.add(house);
-  //   }
-  // });
-
-  // signSet.forEach(sign => {
-  //   console.log(sign);
-  //   const numOfSign = findStellia(signArr, sign);
-  //   console.log(`${numOfSign} placements in ${sign}`);
-  // });
-
-  // houseSet.forEach(house => {
-  //   console.log(house);
-  //   const numOfHouse = findStellia(houseArr, house);
-  //   console.log(`${numOfHouse} placements in house # ${house}`);
-  // });
-
-  /* <-- Return UI --> */
-  return (
     <>
       <article>
-        <h2>Chart of The Moment</h2>
-        <p>Zodiac: {horoscope._zodiac}</p>
-        <p>House System: {horoscope._houseSystem}</p>
-        <>{planets.map((p) => (<p key={p.key}>{p.key} is in {p.Sign.key} in House #{p.House.id}</p>))}</>
+        {!submit ? (
+          <>
+            <h2>Enter Birth Info</h2>
+            <form action='' id='birth-data'>
+              <div className="date">
+                <label htmlFor='year' className='date'>Year:</label>
+                <input type='number' id='year' className='date' name='year' value={formValues.year} onChange={handleChange} />
+                <label htmlFor='month' className='date'>Month:</label>
+                <select id='month' className='date' name='month' value={formValues.month} onChange={handleChange}>
+                  {months.map((month) => <option key={month.value} value={month.value}>{month.label}</option>)}
+                </select>
+                <label htmlFor='day' className='date'>Day:</label>
+                <select id='day' className='date' name='day' value={formValues.day} onChange={handleChange}>
+                  {days.map((day) => <option key={day} value={day}>{day}</option>)}
+                </select>
+              </div>
+
+              <div className="time">
+                <label htmlFor='hour' className='time'>Hour:</label>
+                <select id='hour' className='time' name='hour' value={formValues.hour} onChange={handleChange} disabled={unknown}>
+                  {hours.map((hour) => <option key={hour.value} value={hour.value}>{hour.label}</option>)}
+                </select>
+                <label htmlFor='minute' className='time'>Minute:</label>
+                <select id='minute' className='time' name='minute' value={formValues.minute} onChange={handleChange} disabled={unknown}>
+                  {minutes.map((min) => <option key={min} value={min}>{min}</option>)}
+                </select>
+                <label htmlFor='unknown' className='unknown'>Unknown:</label>
+                <input type='checkbox' id='unknown' className='unknown' name='unknown' value={unknown} onChange={() => setUnknown(unknown => !unknown)}></input>
+              </div>
+
+
+              <div className="location">
+                <label htmlFor='latitude' className='location'>Latitude:</label>
+                <input type='number' id='latitude' className='location' name='latitude' min='-90.00' max='90.00' value={formValues.latitude} onChange={handleChange} />
+                <label htmlFor='longitude' className='location'>Longitude:</label>
+                <input type='number' id='longitude' className='location' name='longitude' min='-90.00' max='90.00' value={formValues.longitude} onChange={handleChange} />
+                <button type='button' id='useLocation' className='location' name='useLocation' onClick={() => setLocation(useLocation => !useLocation)}>Use my Location</button>
+              </div>
+
+              <div className="system">
+                <label htmlFor='zodiac' className='system'>Zodiac:</label>
+                <select id='zodiac' className='system' name='zodiac' value={formValues.zodiacType} onChange={handleChange}>
+                  {zodiacs.map((zodiac) => <option key={zodiac.value} value={zodiac.value}>{zodiac.label}</option>)}
+                </select>
+                <label htmlFor='houseSystem' className='system'>System:</label>
+                <select id='houseSystem' className='system' name='houseSystem' value={formValues.zodiacType} onChange={handleChange}>
+                  {houseSystems.map((system) => <option key={system.value} value={system.value}>{system.label}</option>)}
+                </select>
+              </div>
+              <button type='button' id='submit' name='submit' onClick={() => setSubmit(true)}>Calculate Chart</button>
+            </form>
+          </>
+        ) : (
+          <CreateHoroscope chartData={birthData} />
+        )}
       </article>
+      <CreateHoroscope chartData={MOMENTDATA} />
     </>
   );
 }
@@ -401,6 +397,22 @@ CreateHoroscope.propTypes = {
 AstroDisplay.propTypes = {
   horoscope: object,
   unknown: bool,
+}
+GetDateTimeLocation.propTypes = {
+  year: number,
+  month: number,
+  day: number,
+  hour: number,
+  minute: number,
+  latitude: string,
+  longitude: string,
+}
+GetDegreesMinutes.propTypes = {
+  degreesFormatted: string,
+}
+GetMethods.propTypes = {
+  zodiac: string,
+  houseSystem: string,
 }
 
 /* <- Defaults -> */
